eaded4b84cb7daf45e7522122255ae80f20abeff
JoaquinC539 <joaquinangel@gmail.com>
2024-12-18 12:59:01 -0600

    file zipper

--- esbuild.config.js
+++ esbuild.config.js
@@ -5,6 +5,7 @@ const archiver = require("archiver");
 const outputFile="server.js"
 const outputDir="server"
 const configFileName="server.config.json"
+const outputZip="server.zip"
 function zipOutput(){
     const configPath = path.resolve(__dirname, "server.config.json");
     const config=JSON.parse(fs.readFileSync(configPath,"utf-8"))
@@ -20,14 +21,28 @@ function zipOutput(){
     if(!rootDir){
         throw new Error("Root element does not exist");
     }
-    fs.ensureDirSync(outputDir);
-    fs.copySync(path.join(__dirname,outputFile),path.resolve(__dirname,outputDir));
-    fs.copySync(path.join(__dirname,configFileName),path.resolve(__dirname,outputDir));
-    fs.copySync(path.join(__dirname,config["root"]),path.resolve(__dirname,outputDir),{
+    
+    
+    fs.copySync(path.join(__dirname,outputFile),path.resolve(__dirname,outputDir,outputFile));
+    fs.copySync(path.join(__dirname,configFileName),path.resolve(__dirname,outputDir,configFileName));
+    fs.copySync(path.join(__dirname,config["root"]),path.resolve(__dirname,outputDir,config["root"]),{
         overwrite:true
     });
-    
+    const output=fs.createWriteStream(path.join(__dirname,"/",outputZip) );
     const archive=archiver("zip",{zlib:{level:9}});
+    output.on("close",()=>{
+        console.log(`Archive created: ${outputZip} (${archive.pointer()} bytes)`)
+    });    
+    output.on('end', function() {
+        console.log('Data has been drained');
+      });
+
+    archive.on("error",(err)=>{
+        throw err;
+    });
+    archive.pipe(output);
+    archive.directory(outputDir,false);
+    archive.finalize();
 }
 
 esbuild.build({
@@ -39,6 +54,6 @@ esbuild.build({
     sourcemap:false,
     minify:true
 }).then(()=>{    
-    console.log("Bundle complete")
+    console.log("Bundle complete");
     zipOutput();
 }).catch(()=>process.exit(1))
