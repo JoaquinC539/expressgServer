69ce451bea910f658645d89f5fcc8aa1bc4b9749
JoaquinC539 <joaquinangel@gmail.com>
2024-12-20 11:51:17 -0600

    updated bundler missing some lines

--- esbuild.config.js
+++ esbuild.config.js
@@ -7,49 +7,71 @@ const outputDir="server"
 const configFileName="server.config.json"
 const outputZip="server.zip"
 const outputZipTar="server.tar.gz"
-function zipOutput(){
+function zipOutput(){    
+    if(!fs.existsSync(configFileName)){
+        console.error("Config file does not exist");
+        throw new Error("Config file does not exist");
+    }
     const configPath = path.resolve(__dirname, "server.config.json");
     const config=JSON.parse(fs.readFileSync(configPath,"utf-8"))
     const outpt=fs.existsSync(outputFile);
     if(!outpt){
-        throw new Error("Output does not exist")
+        console.error("Output does not exist");
+        throw new Error("Output does not exist");
     }
     const rootDir=fs.existsSync(config["root"])
     if(!rootDir){
-        throw new Error("Root element does not exist");
-    }
-    const configDir=fs.existsSync(configFileName)
-    if(!rootDir){
+        console.error("Root element does not exist");
         throw new Error("Root element does not exist");
     }
     
+
+    if(fs.existsSync(outputDir)){
+        fs.rmSync(outputDir,{recursive:true,force:true}); 
+     }
+     fs.mkdirSync(outputDir,{recursive:true})
     
     fs.copySync(path.join(__dirname,outputFile),path.resolve(__dirname,outputDir,outputFile));
     fs.copySync(path.join(__dirname,configFileName),path.resolve(__dirname,outputDir,configFileName));
     fs.copySync(path.join(__dirname,config["root"]),path.resolve(__dirname,outputDir,config["root"]),{
         overwrite:true
     });
-    let output=fs.createWriteStream(path.join(__dirname,"/",outputZip) );
-    let archive=archiver("zip",{zlib:{level:9}});
-    output.on("close",()=>{
-        console.log(`Archive created: ${outputZip} (${archive.pointer()} bytes)`)
-    });    
-    output.on('end', function() {
-        console.log('Data has been drained');
-      });
 
-    archive.on("error",(err)=>{
+    // Create ZIP archive
+    let zipOutput = fs.createWriteStream(path.join(__dirname, outputZip));
+    let zipArchive = archiver("zip", { zlib: { level: 9 } });
+
+    zipOutput.on("close", () => {
+        console.log(`ZIP Archive created: ${outputZip} (${zipArchive.pointer()} bytes)`);
+    });
+    zipOutput.on("end", () => {
+        console.log("ZIP Archive data has been drained.");
+    });
+    zipArchive.on("error", (err) => {
         throw err;
     });
-    archive.pipe(output);
-    archive.directory(outputDir,false);
-    archive.finalize();
 
-    output=fs.createWriteStream(path.join(__dirname,"/",outputZipTar) );
-    archive=archiver("tar",{zlib:{level:9}})
-    archive.pipe(output);
-    archive.directory(outputDir,false);
-    archive.finalize();
+    zipArchive.pipe(zipOutput);
+    zipArchive.directory(outputDir, false);
+    zipArchive.finalize();
+
+    // Create TAR file
+    let tarOutput = fs.createWriteStream(path.join(__dirname, outputZipTar));
+    let tarArchive = archiver("tar", { zlib: { level: 9 } });
+
+    tarOutput.on("close", () => {
+        console.log(`TAR.GZ Archive created: ${outputZipTar} (${tarArchive.pointer()} bytes)`);
+    });
+    tarOutput.on("end", () => {
+        console.log("TAR.GZ Archive data has been drained.");
+    });
+    tarArchive.on("error", (err) => {
+        throw err;
+    });
+
+    tarArchive.pipe(tarOutput);
+    tarArchive.directory(outputDir, false);
+    tarArchive.finalize();
     
 }
 
