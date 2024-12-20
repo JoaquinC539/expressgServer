ecba96c3a98a5daebfbed24004d20fc74d7243cc
JoaquinC539 <joaquinangel@gmail.com>
2024-12-20 11:27:21 -0600

    updated server to use abs path and cleaning

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const express = require("express")
 const path = require("path");
 const fs = require("fs");
 const bodyParser = require("body-parser");
-const cookieParser=require("cookie-parser");
-const cors=require("cors")
+const cookieParser = require("cookie-parser");
+const cors = require("cors")
 
 const configPath = path.resolve(__dirname, "server.config.json");
 const config = JSON.parse(fs.readFileSync(configPath, "utf-8"))
-const corsConfig={
-    origin:function(origin,callback){
-        if(config['allowed-origins'].indexOf(origin) !==-1 ||!origin){
-            callback(null,true);
-        }else{
-            callback(new Error("Not allowed by CORS"));
+const corsConfig = {
+    origin: config['allowed-origins'].includes("*")
+        ? true
+        : function (origin, callback) {
+
+            if (config['allowed-origins'].indexOf(origin) !== -1 || !origin) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
         }
-    }
 }
 
 const app = express();
@@ -22,23 +25,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(cors(corsConfig))
-
-app.use(express.static(config['root']));
-
+const rootPath = path.resolve(__dirname, config['root']);
+app.use(express.static(rootPath));
 if (config["fallBackToIndex"]) {
-    app.use((req,res,next)=>{
-        const indexPath=path.join(__dirname,config['root'],config["entryPoint"]);
-        if(fs.existsSync(indexPath)){
+    app.use((req, res, next) => {
+        const indexPath = path.join(rootPath, config["entryPoint"]);
+        if (fs.existsSync(indexPath)) {
             res.sendFile(indexPath)
-        }else{
-            res.status(404).send("404 Not Found")
+        } else {
+            next();
+            // res.status(404).send("404 Not Found")
         }
     });
 }
 
-app.listen(config["port"],()=>{
+app.listen(config["port"], () => {
     console.log(`Server is running on  http://localhost:${config["port"]}`);
-    console.log(`Serving static files from ${config['root']}`)
+    console.log(`Serving static files from ${rootPath}`)
+    console.log(`Serving index at ${path.join(rootPath, config["entryPoint"])}`)
 })
 
 
